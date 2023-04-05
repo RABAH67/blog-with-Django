6c5cde66ac4cc67af3c46d9e4c49515342d5b583
rabah <rabah@gmail.com>
2023-04-05 07:05:37 -0700

    add post && About page

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,3 +7,11 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'blog/index.html')
+
+
+
+
+
+def about(request):
+    
+    return render(request, 'blog/about.html')
